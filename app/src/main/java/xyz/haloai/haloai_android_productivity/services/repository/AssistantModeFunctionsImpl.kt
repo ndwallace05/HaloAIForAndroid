d4d20e210e37b79a587c8b9ebfd44616e622a931
rnagotkar <rnagotkar@seattleu.edu>
2024-07-14 15:58:06 -0700

    voice assistant features for prompt

--- app/src/main/java/xyz/haloai/haloai_android_productivity/services/repository/AssistantModeFunctionsImpl.kt
+++ app/src/main/java/xyz/haloai/haloai_android_productivity/services/repository/AssistantModeFunctionsImpl.kt
@@ -20,25 +20,79 @@ class AssistantModeFunctionsImpl(private val assistantModeFunctions: AssistantMo
     // Business Logic
     override suspend fun ask_ai(conversation: List<ChatHistory.Message>): String {
         // Ask the AI a question
-        val rawResponse = assistantModeFunctions.ask_ai(conversation.last().text)
-        val parsedResponse = assistantModeFunctions.parse_ai_response(rawResponse)
+//        val rawResponse = assistantModeFunctions.ask_ai(conversation)
+        val functionsList = assistantModeFunctions.convert_chat_to_functions(conversation)
+        val parsedResponse = assistantModeFunctions.parseAssistantOutput(functionsList)
+
         var finalResponse = ""
+        val variableMap = mutableMapOf<String, String>()
+
         for (instruction in parsedResponse)
         {
-            if (instruction.function == EnumFunctionTypes.ADD_TO_NOTES)
-            {
-                assistantModeFunctions.addToNotes(instruction.params["note"])
+            val arguments: MutableList<String> = mutableListOf()
+            for (args in instruction.params){
+                //if argument is of format $1, $2, etc (using regex).
+                val regex = Regex("\\$\\d+")
+                if (regex.containsMatchIn(args.value)) {
+                    arguments.add(variableMap[args.value] ?: "")
+                }
+                else {
+                    arguments.add(args.value)
+                }
+
             }
-            else if (instruction.function == EnumFunctionTypes.SEND_EMAIL)
-            {
-                assistantModeFunctions.sendEmail(instruction.params["email"])
-            }
-            else if (instruction.function == EnumFunctionTypes.RESPOND_TO_USER)
-            {
-                finalResponse = instruction.params["response"].toString()
+
+            when (instruction.function){
+                EnumFunctionTypes.CREATE_REMINDER -> {
+                    assistantModeFunctions.createReminder(reminderText = arguments[0].toString(), dateAndTimeForReminder = arguments[1].toString())
+                }
+
+                EnumFunctionTypes.ADD_TO_NOTES -> {
+
+                    assistantModeFunctions.addToNote(noteText = arguments[0].toString(), noteTitle = arguments[1].toString())
+                }
+                EnumFunctionTypes.RESPOND_TO_USER -> {
+                    finalResponse = assistantModeFunctions.respondToUser(responseText = arguments[0].toString())
+                }
+                EnumFunctionTypes.CREATE_UNSCHEDULED_REMINDER -> {
+                    assistantModeFunctions.createUnscheduledReminder(reminderText = arguments[0].toString())
+                }
+                EnumFunctionTypes.GET_NOTE_TITLES -> {
+                    // add output of function to variable map
+                    variableMap[instruction.varNumberToStoreIn!!] = assistantModeFunctions.getNoteTitles().toString()
+                }
+                EnumFunctionTypes.GET_AVAILABLE_TIMES -> {
+                    variableMap[instruction.varNumberToStoreIn!!] = assistantModeFunctions.getAvailableTimes(day = arguments[0].toString(),
+                        currentDateTime = arguments[1].toString()
+                    )
+                }
+                EnumFunctionTypes.CREATE_EVENT -> {
+                    assistantModeFunctions.createCalendarEvent(
+                        text = arguments[0].toString(),
+                        description = arguments[1].toString(),
+                        startDateTime = arguments[2].toString(),
+                        endDateTime = arguments[3].toString()
+                    )
+                }
+                EnumFunctionTypes.SUMMARIZE_TEXT -> {
+                    variableMap[instruction.varNumberToStoreIn!!] = assistantModeFunctions.summarizeText()
+                }
+                EnumFunctionTypes.ASK_AI -> {
+                    variableMap[instruction.varNumberToStoreIn!!] = assistantModeFunctions.askAi(prompt = arguments[0].toString())
+                }
+                EnumFunctionTypes.GET_CONTACT -> {
+                    variableMap[instruction.varNumberToStoreIn!!] = assistantModeFunctions.getContact(name = arguments[0].toString())
+                }
+                EnumFunctionTypes.SAVE_CONTACT -> {
+                    assistantModeFunctions.saveContact(
+                        name = arguments[0].toString(),
+                        email = arguments[1].toString(),
+                        phone = arguments[2].toString()
+                    )
+                }
             }
         }
-        return rawResponse
+        return finalResponse
     }
 
 }
